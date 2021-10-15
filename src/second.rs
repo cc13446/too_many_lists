18447e82e86fad295db80aa57b193e2b876d746c
cc13446 <1344613920@qq.com>
2021-10-15 10:11:48 +0800

    3.6 可变迭代器 &的copy方法 唯一可变引用

--- src/second.rs
+++ src/second.rs
@@ -15,6 +15,10 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
+pub struct IterMut<'a, T> {
+    next : Option<&'a mut Node<T>>
+}
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List {
@@ -59,6 +63,20 @@ impl<T> List<T> {
         }
     }
 
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut()
+        }
+    }
+
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
@@ -72,6 +90,17 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
+impl<'a, T> Iterator for IterMut<'a, T> {
+    // trait 要求必须实现Item
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+
 impl<T> Drop for List<T> {
     fn drop(&mut self) {
         let mut cur_link = self.head.take();
@@ -81,17 +110,8 @@ impl<T> Drop for List<T> {
     }
 }
 
-impl<T> Iterator for IntoIter<T> {
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        // access fields of a tuple struct numerically
-        self.0.pop()
-    }
-}
-
-
 mod test {
-    use crate::first::List;
+    use crate::second::List;
 
     #[test]
     fn basics() {
@@ -121,48 +141,59 @@ mod test {
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
     }
-}
 
-#[test]
-fn peek() {
-    let mut list = List::new();
-    assert_eq!(list.peek(), None);
-    assert_eq!(list.peek_mut(), None);
-    list.push(1);
-    list.push(2);
-    list.push(3);
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
 
-    assert_eq!(list.peek(), Some(&3));
-    assert_eq!(list.peek_mut(), Some(&mut 3));
-    list.peek_mut().map(|value| {
-        *value = 42
-    });
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| {
+            *value = 42
+        });
 
-    assert_eq!(list.peek(), Some(&42));
-    assert_eq!(list.pop(), Some(42));
-}
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
+    }
 
-#[test]
-fn into_iter() {
-    let mut list = List::new();
-    list.push(1); list.push(2); list.push(3);
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
 
-    let mut iter = list.into_iter();
-    assert_eq!(iter.next(), Some(3));
-    assert_eq!(iter.next(), Some(2));
-    assert_eq!(iter.next(), Some(1));
-    assert_eq!(iter.next(), None);
-}
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
 
-#[test]
-fn iter() {
-    let mut list = List::new();
-    list.push(1); list.push(2); list.push(3);
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
 
-    let mut iter = list.iter();
-    assert_eq!(iter.next(), Some(&3));
-    assert_eq!(iter.next(), Some(&2));
-    assert_eq!(iter.next(), Some(&1));
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+    }
 }
 
 
